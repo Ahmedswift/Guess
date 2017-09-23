e2821e4da697362c37659d0dcb0e9dd28a9ccb0b
Ahmedswift <amd25it@gmail.com>
2017-09-23 23:57:49 +0300

    Initial Commit

---
+++ Guess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guess
+//
+//  Created by Ahmad Ahrbi on 1/3/1439 AH.
+//  Copyright © 1439 Ahmad Ahrbi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
